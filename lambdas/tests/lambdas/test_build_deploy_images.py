a0efcc9730c498a44ee6e9841f7a4cb474cb5814
Jeremy Watt <jermwatt@gmail.com>
2024-08-29 14:15:51 -0700

    starting cloud_decopuled test refactoring

---
+++ lambdas/tests/lambdas/test_build_deploy_images.py
@@ -0,0 +1,22 @@
+import pytest
+import os
+from tests.utilities.execute_subprocess import execute_subprocess_command
+
+current_directory = os.getcwd()
+
+STAGE = "test"
+receivers = ["receiver_start", "receiver_preprocess", "receiver_process", "receiver_end"]
+
+
+@pytest.mark.parametrize("RECEIVER_NAME", receivers)
+def test_build_image(RECEIVER_NAME):
+    # build image
+    command = ["bash", "build_image.sh", STAGE, RECEIVER_NAME]
+    stdout = execute_subprocess_command(command, cwd=current_directory + "/lambdas/build_deploy_scripts")
+
+
+@pytest.mark.parametrize("RECEIVER_NAME", receivers)
+def test_deploy_image(RECEIVER_NAME):
+    # deploy image
+    command = ["bash", "deploy_image.sh", STAGE, RECEIVER_NAME]
+    stdout = execute_subprocess_command(command, cwd=current_directory + "/lambdas/build_deploy_scripts")
