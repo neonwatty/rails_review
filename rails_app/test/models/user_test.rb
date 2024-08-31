b717e8bd98dbf5b384ff56eea1cae54af904c886
Jeremy Watt <jermwatt@gmail.com>
2024-08-31 12:05:16 -0700

    running rails side tests - users

--- rails_app/test/models/user_test.rb
+++ rails_app/test/models/user_test.rb
@@ -6,23 +6,29 @@ class UserTest < ActiveSupport::TestCase
     assert user.save
   end
 
-  # test "should not create a new user with an existing email" do
-  #   user = User.find(1)
-  #   new_user = User.new(email: user.email, password: "password", password_confirmation: "password")
-  #   assert_not new_user.save
-  # end
-  # test "should not create a new user without email" do
-  #   user = User.new(password: "password", password_confirmation: "password")
-  #   assert_not user.save
-  # end
+  test "should not create a new user with an existing email" do
+    user = User.first
+    new_user = User.new(email: user.email, password: "password", password_confirmation: "password")
+    assert_not new_user.save
+  end
 
-  # test "should not create a new user without password" do
-  #   user = User.new(email: "user4@example.com")
-  #   assert_not user.save
-  # end
+  test "should not create a new user without email" do
+    user = User.new(password: "password", password_confirmation: "password")
+    assert_not user.save
+  end
 
-  # test "should not create a new user with a password less than 6 characters" do
-  #   user = User.new(email: "user5@example.com", password: "pass", password_confirmation: "pass")
-  #   assert_not user.save
-  # end
+  test "should not create a new user without password" do
+    user = User.new(email: "user3@example.com")
+    assert_not user.save
+  end
+
+  test "should not create a new user with a password less than 8 characters" do
+    user = User.new(email: "user3@example.com", password: "pass", password_confirmation: "pass")
+    assert_not user.save
+  end
+
+  test "should not create a new user without password confirmation" do
+    user = User.new(email: "user3@example.com", password: "password")
+    assert_not user.save
+  end
 end
