748185ee02d820a5041238d72efafad41a171ca1
Jeremy Watt <jermwatt@gmail.com>
2024-08-31 12:59:03 -0700

    running rails side tests - upload

--- rails_app/test/models/upload_test.rb
+++ rails_app/test/models/upload_test.rb
@@ -1,13 +1,30 @@
 require "test_helper"
 
 class UploadTest < ActiveSupport::TestCase
-  test "should save upload" do
-    upload = Upload.create(id: 1, user_id: 1, file: "file")
-    assert upload.save
+  # Load the fixtures
+  fixtures :users
+
+  # by default, model saves upload with user_id=1
+  # test "should not save upload without user" do
+  #   upload = Upload.new
+  #   upload.files.attach(io: File.open(Rails.root.join("test/fixtures/files/mississippi_john_hurt.png")), filename: "mississippi_john_hurt.png")
+  #   assert_not upload.save, "Saved the upload without a user"
+  # end
+
+  test "should not save upload without files" do
+    upload = Upload.new(user: users(:one))
+    assert_not upload.save, "Saved the upload without files"
   end
 
-  test "should not save upload without user_id" do
-    upload = Upload.create(id: 1, file: "file")
-    assert_not upload.save
+  test "should associate upload with user" do
+    upload = Upload.new(user: users(:one))
+    upload.files.attach(io: File.open(Rails.root.join("test/fixtures/files/mississippi_john_hurt.png")), filename: "mississippi_john_hurt.png")
+    assert_equal users(:one), upload.user, "Upload is not associated with the correct user"
+  end
+
+  test "should have attached files" do
+    upload = Upload.new(user: users(:one))
+    upload.files.attach(io: File.open(Rails.root.join("test/fixtures/files/mississippi_john_hurt.png")), filename: "mississippi_john_hurt.png")
+    assert upload.files.attached?, "Upload should have files attached"
   end
 end
