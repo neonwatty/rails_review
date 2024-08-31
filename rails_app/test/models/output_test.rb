9a1d18167b77d7b9be80d26a7587764c1910a008
Jeremy Watt <jermwatt@gmail.com>
2024-08-31 10:57:28 -0700

    running rails side tests

--- rails_app/test/models/output_test.rb
+++ rails_app/test/models/output_test.rb
@@ -4,4 +4,6 @@ class OutputTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
 end
--- rails_app/test/models/upload_test.rb
+++ rails_app/test/models/upload_test.rb
@@ -4,4 +4,9 @@ class UploadTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save upload without file" do
+    upload = Upload.new
+    assert_not upload.save
+  end
 end
