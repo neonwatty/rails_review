f346ce766566d1c1b6e96cba4cf16989ba522f13
Jeremy Watt <jermwatt@gmail.com>
2024-08-31 12:15:02 -0700

    running rails side tests - upload

--- rails_app/test/models/upload_test.rb
+++ rails_app/test/models/upload_test.rb
@@ -1,12 +1,13 @@
 require "test_helper"
 
 class UploadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save upload" do
+    upload = Upload.create(id: 1, user_id: 1, file: "file")
+    assert upload.save
+  end
 
-  test "should not save upload without file" do
-    upload = Upload.create(id: 2, user_id: 1)
+  test "should not save upload without user_id" do
+    upload = Upload.create(id: 1, file: "file")
     assert_not upload.save
   end
 end
