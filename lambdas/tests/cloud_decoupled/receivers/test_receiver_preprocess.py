0a47af48589b5f8767a1af8eb471011c1d5d3feb
Jeremy Watt <jermwatt@gmail.com>
2024-08-30 17:57:45 -0700

    cleaning up namespaces

--- lambdas/tests/cloud_decoupled/receivers/test_receiver_preprocess.py
+++ lambdas/tests/cloud_decoupled/receivers/test_receiver_preprocess.py
@@ -58,7 +58,7 @@ def build_deploy():
 def test_success(build_deploy, subtests):
     print("INFO: starting test_success")
     ### setup step ###
-    file_id, request_id, s3_key, s3_key_save, receiver_receipt_handle = step_setup(subtests, test_file_name, test_file_path, RECEIVER_NAME, step_progress="in_progress")  
+    file_id, request_id, s3_key, s3_key_save, receiver_receipt_handle = step_setup(subtests, test_file_name, test_file_path, BUCKET_TEST, TEST_RECEIVERS_QUEUE, RECEIVER_NAME, step_progress="in_progress")  
     event = s3sqs_event_maker(BUCKET_TEST, s3_key, TEST_RECEIVERS_QUEUE, receiver_receipt_handle)
     print(f"sqs event maker complete")
     
