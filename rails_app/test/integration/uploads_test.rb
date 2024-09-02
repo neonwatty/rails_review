69fd10dda2b82e2c2683fad95463aef60ec4d0ec
Jeremy Watt <jermwatt@gmail.com>
2024-09-02 10:37:40 -0700

    running rails side tests - integrations

--- rails_app/test/integration/uploads_test.rb
+++ rails_app/test/integration/uploads_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 class BlogFlowTest < ActionDispatch::IntegrationTest
   # sign in with first user
-  setup 
-
   setup do
     # Log in the user from the fixture
     @user = users(:one)
@@ -27,18 +25,19 @@ class BlogFlowTest < ActionDispatch::IntegrationTest
 
   test "should create upload" do
     assert_difference('Upload.count') do
-      post uploads_path, params: { upload: { files: fixture_file_upload('files/test_image.jpg') } }
+      post uploads_path, params: { upload: { files: fixture_file_upload('cover_image.jpeg') } }
+      assert_redirected_to upload_path(Upload.last)
     end
     assert_redirected_to upload_path(Upload.last)
     assert_not_nil flash[:notice]
   end
 
-  #   post "/uploads",
-  #     params: { upload: { user_id: 1, files_attached: false } }
-  #   assert_response :redirect
-  #   follow_redirect!
-  #   assert_response :success
-  #   assert_select "p", "Upload was successfully created."
-
+  test "should destroy upload" do
+    assert_difference('Upload.count', -1) do
+      delete upload_path(Upload.last)
+    end
+    assert_redirected_to uploads_path
+    assert_not_nil flash[:notice]
+  end
 
 end
