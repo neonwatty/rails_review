fbd721a182a4c1f6f03cf9123afe97f62ed4696b
Jeremy Watt <jermwatt@gmail.com>
2024-09-04 07:38:34 -0700

    Update test_s3_create.py

--- lambdas/tests/aws_scaffold/test_s3_create.py
+++ lambdas/tests/aws_scaffold/test_s3_create.py
@@ -1,19 +1,29 @@
 import pytest
 from aws_scaffold.s3.create import create_bucket
+from aws_scaffold.s3.add_lifecycle import add_lifecycle_expire
 from aws_scaffold.s3.delete import delete_bucket
 import random
 import time
 
-custom_charset = 'abcdefghijklmnopqrstuvwxyz'
+custom_charset = "abcdefghijklmnopqrstuvwxyz"
+
 
 def generate_custom_random_string(length):
-    return ''.join(random.choices(custom_charset, k=length))
+    return "".join(random.choices(custom_charset, k=length))
+
 
 bucket_names = [f"test-bucket-{generate_custom_random_string(7)}"]
+
 
 @pytest.mark.parametrize("bucket_name", bucket_names)
 def test_create_bucket(bucket_name):
     create_val = create_bucket(bucket_name)
+    assert create_val == True
+
+
+@pytest.mark.parametrize("bucket_name", bucket_names)
+def test_add_expire(bucket_name):
+    create_val = add_lifecycle_expire(bucket_name)
     assert create_val == True
 
 
