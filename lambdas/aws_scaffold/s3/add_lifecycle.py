c9a0960a5f7802db1cef9ddfa8ebc0f1815c6c19
Jeremy Watt <jermwatt@gmail.com>
2024-09-04 07:39:16 -0700

    Create add_lifecycle.py

---
+++ lambdas/aws_scaffold/s3/add_lifecycle.py
@@ -0,0 +1,28 @@
+from aws_scaffold import session
+
+s3_client = session.client("s3", region_name="us-west-2")
+
+
+def add_lifecycle_expire(bucket_name: str, num_days: int = 1) -> bool:
+    try:
+        # Define the lifecycle configuration
+        lifecycle_config_settings = {
+            'Rules': [{
+                "ID": "ExpireObjectsAfterOneDay",
+                "Expiration": {
+                    "Days": 1
+                },
+                'Status': 'Enabled',
+                'Filter': {}
+            }
+        ]}
+        
+        # Apply the lifecycle configuration to the bucket
+        s3_client.put_bucket_lifecycle_configuration(Bucket=bucket_name, LifecycleConfiguration=lifecycle_config_settings)
+
+        print(f"SUCCESS: lifecycle expire rule added to bucket {bucket_name}")
+        return True
+    except Exception as e:
+        failure_message = f"FAILURE: lifecycle expire add falied with exception {e} on bucket {bucket_name}"
+        print(failure_message)
+        return False
