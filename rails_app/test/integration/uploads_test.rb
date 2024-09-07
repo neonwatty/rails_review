6e7edc2bda78351e5c781bfda54af04703b3168e
Jeremy Watt <jermwatt@gmail.com>
2024-09-07 05:50:51 -0700

    baseline integration upload test update

--- rails_app/test/integration/uploads_test.rb
+++ rails_app/test/integration/uploads_test.rb
@@ -36,22 +36,10 @@ class BlogFlowTest < ActionDispatch::IntegrationTest
     assert_redirected_to upload_path(Upload.last)
     assert_not_nil flash[:notice]
 
-    # Define maximum attempts and wait time between checks
-    max_attempts = 15
-    wait_time = 3 # seconds
-    final_status = nil
-
-    max_attempts.times do |attempt|
-      sleep(wait_time) # Wait for the specified duration
-
-      status_record = Status.find_by(upload_id: Upload.last)
-      final_status = { start: status_record.receiver_start, preprocess: status_record.receiver_preprocess, process: status_record.receiver_process }
-
-      # Break the loop if status is complete
-      if final_status[:start] == 'complete' && final_status[:preprocess] == 'complete' && final_status[:process] == 'complete'
-        break
-      end
-    end
+    # sleep for X secs and query for status completion
+    sleep(45)
+    status_record = Status.find_by(upload_id: Upload.last)
+    final_status = { start: status_record.receiver_start, preprocess: status_record.receiver_preprocess, process: status_record.receiver_process }
 
     # Assert that the final status is complete after all attempts
     assert_equal 'complete', final_status[:start], "Expected 'complete' for receiver_start but got #{final_status[:start]}"
@@ -59,21 +47,6 @@ class BlogFlowTest < ActionDispatch::IntegrationTest
     assert_equal 'complete', final_status[:process], "Expected 'complete' for receiver_process but got #{final_status[:process]}"
 
     # Assert that the ActiveStorage blobs count is 2
-    max_attempts = 10
-    wait_time = 3 # seconds
-    blob_count = nil
-
-    max_attempts.times do |attempt|
-      sleep(wait_time) # Wait for the specified duration
-
-      blob_count =  ActiveStorage::Blob.count
-
-      # Break the loop if status is complete
-      if blob_count == 2
-        break
-      end
-    end
-
     assert_equal 2, ActiveStorage::Blob.count, "Expected 2 blobs but found #{ActiveStorage::Blob.count}"
   end
 
