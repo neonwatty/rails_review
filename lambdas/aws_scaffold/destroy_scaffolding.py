b97394a219bc7bb0cd44deb7be0134f49b9b15ea
Jeremy Watt <jermwatt@gmail.com>
2024-09-09 09:29:51 -0700

    working on cors update

--- lambdas/aws_scaffold/destroy_scaffolding.py
+++ lambdas/aws_scaffold/destroy_scaffolding.py
@@ -1,11 +1,15 @@
+import os
 import pytest
 from aws_scaffold import APP_NAME_PRIVATE
 from aws_scaffold import receiver_names
+from aws_scaffold.generators.generate_namespaces import connected_stages
 from aws_scaffold.generators.generate_namespaces import generate_bucket_names
 from aws_scaffold.generators.generate_namespaces import generate_queue_names
 from aws_scaffold.s3.delete import delete_bucket
 from aws_scaffold.sqs.delete import delete_sqs_queue
 from aws_scaffold.ecr.delete import delete_ecr_repository
+from tests.utilities.execute_subprocess import execute_subprocess_command
+current_directory = os.getcwd()
 
 
 # delete bucket names
@@ -33,3 +37,13 @@ def test_delete_ecr_repos(receiver_name):
     repo_name = f"{APP_NAME_PRIVATE}-{receiver_name}"
     create_val = delete_ecr_repository(repo_name)
     assert create_val == True
+    
+# delete receiver lambdas
+@pytest.mark.parametrize("stage", connected_stages)
+def test_delete_receiver_lambdas(stage):
+    # deploy serverless config for receivers
+    config = "serverless_receivers.yml"
+    print(f"INFO: deploying services in {config} to stage {stage}")
+    command = ["bash", "adjust_functions.sh", "deploy", stage, config]
+    stdout = execute_subprocess_command(command, cwd=current_directory + "/lambdas")
+    print("INFO: ...done!")
