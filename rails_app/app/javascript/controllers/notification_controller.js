086c2a5dad3a6a871dcbe1010be30694dc19a202
Jeremy Watt <jermwatt@gmail.com>
2024-09-09 19:30:25 -0700

    fixing flash messages

--- rails_app/app/javascript/controllers/notification_controller.js
+++ rails_app/app/javascript/controllers/notification_controller.js
@@ -3,6 +3,5 @@ import Notification from '@stimulus-components/notification'
 export default class extends Notification {
   connect() {
     super.connect()
-    console.log('Do what you want here.')
   }
 }
