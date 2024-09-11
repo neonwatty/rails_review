de6f07c34e85d39acb2b2ce47d48de7c994ea8a4
Jeremy Watt <jermwatt@gmail.com>
2024-09-11 08:44:54 -0700

    adjusting dropzone controller

--- rails_app/app/javascript/controllers/dropzone_controller.js
+++ rails_app/app/javascript/controllers/dropzone_controller.js
@@ -1,13 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 import { DirectUpload } from "@rails/activestorage";
 import Dropzone from "dropzone";
-import {
-  findElement,
-  getMetaValue,
-  insertAfter,
-  removeElement,
-  toArray
-} from "../helpers";
+
 
 export default class extends Controller {
   static targets = ["input"];
@@ -152,4 +146,40 @@ function createDropZone(controller) {
     addRemoveLinks: controller.addRemoveLinks,
     autoQueue: false
   });
+}
+
+
+export function getMetaValue(name) {
+  const element = findElement(document.head, `meta[name="${name}"]`);
+  if (element) {
+    return element.getAttribute("content");
+  }
+}
+
+export function findElement(root, selector) {
+  if (typeof root == "string") {
+    selector = root;
+    root = document;
+  }
+  return root.querySelector(selector);
+}
+
+export function toArray(value) {
+  if (Array.isArray(value)) {
+    return value;
+  } else if (Array.from) {
+    return Array.from(value);
+  } else {
+    return [].slice.call(value);
+  }
+}
+
+export function removeElement(el) {
+  if (el && el.parentNode) {
+    el.parentNode.removeChild(el);
+  }
+}
+
+export function insertAfter(el, referenceNode) {
+  return referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
 }
