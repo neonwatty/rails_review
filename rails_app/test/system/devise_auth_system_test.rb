1d07a8b15728821d39aca2cd634b6d0e6d1bd2a6
Jeremy Watt <jermwatt@gmail.com>
2024-09-16 05:43:01 -0700

    devise system tests - started

--- rails_app/test/system/devise_auth_system_test.rb
+++ rails_app/test/system/devise_auth_system_test.rb
@@ -1,6 +1,11 @@
 require "application_system_test_case"
 
 class DeviseAuthSystemTest < ApplicationSystemTestCase
+  fixtures :all  
+
+  def setup
+    @user = users(:one)  
+  end
 
   test 'test_1: sign in existing user (from seed)' do
     # set to homepage and see if 'Log in' link is present
@@ -10,7 +15,7 @@ class DeviseAuthSystemTest < ApplicationSystemTestCase
     # visit login page and fill in form
     visit new_user_session_path
     assert_selector '#new_user'
-    fill_in 'user_email', with: 'neonwatty@gmail.com'
+    fill_in 'user_email', with: @user.email
     fill_in 'user_password', with: 'password123'
     click_on 'new-session-login-btn'
     assert_current_path root_path
@@ -32,12 +37,42 @@ class DeviseAuthSystemTest < ApplicationSystemTestCase
 
     # visit signup page and fill in form
     visit new_user_registration_path
-    fill_in 'user_email', with: 'dummy@dummy.com'
-    fill_in 'user_password', with: 'password'
-    fill_in 'user_password_confirmation', with: 'password'
+    fill_in 'new-user-email-field', with: 'dummy@dummy.com'
+    fill_in 'new-user-password-field', with: 'password'
+    fill_in 'new-user-password-confirm-field', with: 'password'
     click_on 'signup-registration-btn'
     assert_current_path root_path
   end
 
+  test 'test_4: edit user profile' do 
+    # login
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: 'password123'
+    click_on 'new-session-login-btn'
+    assert_current_path root_path
+
+    # visit edit page
+    visit edit_user_registration_path
+    assert_current_path edit_user_registration_path
+
+    # fill in field based on id
+    fill_in 'edit-profile-email', with: 'blah@blah.com'
+    fill_in 'edit-profile-password-current', with: 'password123'
+    click_on 'update-profile-btn'
+    assert_current_path root_path
 end
-  
+
+  test 'test_5: password reminder' do
+    # visit login page and click on 'Forgot your password?'
+    visit new_user_session_path
+    click_on 'Forgot your password?'
+    assert_current_path new_user_password_path
+
+    # fill in email and click on 'Send me reset password instructions'
+    fill_in 'user_email', with: @user.email
+    click_on 'send-reset-password-btn'
+    assert_current_path new_user_session_path
+  end
+
+end
